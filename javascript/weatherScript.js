5cd3144f51b97021ac2e740bc028fb4fb2579777
dennislee89 <dennislee1989@gmail.com>
2018-02-04 22:59:29 -0600

    Add files via upload
    changed javascript to change image to match weather

--- javascript/weatherScript.js
+++ javascript/weatherScript.js
@@ -6,14 +6,14 @@ function init() {
 function getWeather(){
     jQuery(document).ready(function ($) {
         $.ajax({
-            url: "http://api.wunderground.com/api/ef7bf0e86a6f3e3a/geolookup/conditions/forecast/q/IL/Chicago.json",
+            url: "https://api.wunderground.com/api/ef7bf0e86a6f3e3a/geolookup/conditions/forecast/q/autoip.json",
             dataType: "jsonp",
             success: function (parsed_json) {
               
               //forecast for current day
     var forecast = parsed_json['forecast']['simpleforecast']['forecastday'][0];
     var current = parsed_json['current_observation']['temp_f'];    
-                   
+                  
               
               var day = forecast.date.weekday;
               var date=forecast.date.day;
@@ -28,7 +28,22 @@ function getWeather(){
               document.getElementById('condition').innerHTML = condition ;
               document.getElementById('current').innerHTML = 'Currently: ' + current + '&#x2109';
               document.getElementById('high').innerHTML = 'High: '+ high + '&#x2109';
-              document.getElementById('low').innerHTML ='Low: ' + low + '&#x2109';                                                             
+              document.getElementById('low').innerHTML ='Low: ' + low + '&#x2109';      
+
+
+                if(current > 40) {
+                  document.getElementById("background").classList.add('fall');
+                } else if(current > 60){
+                  document.getElementById("background").classList.add('sunny');
+                } else if(current < 40){
+                  document.getElementById("background").classList.add('snow');
+                } else if (condition == 'rain'){
+                  document.getElementById("background").classList.add('rain');
+                } else {
+                  document.body.style.backgroundImage = "url('../weatherIMG/weather.jpg')";
+                }
+
+
             }
         });
     });
